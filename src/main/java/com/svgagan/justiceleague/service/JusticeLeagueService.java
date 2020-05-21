c3e0bfb5e2e0ca2f87a529113a24f4d3e87f32c7
Gagan Solur Venkatesh <gagan.solur.venkatesh@peapod.com>
2020-05-21 12:09:53 -0500

    Comic Details updates

--- src/main/java/com/svgagan/justiceleague/service/JusticeLeagueService.java
+++ src/main/java/com/svgagan/justiceleague/service/JusticeLeagueService.java
@@ -1,5 +1,6 @@
 package com.svgagan.justiceleague.service;
 
+import com.svgagan.justiceleague.model.ComicDetails;
 import com.svgagan.justiceleague.model.SuperHeroes;
 import com.svgagan.justiceleague.repository.JusticeLeagueRepository;
 import org.springframework.stereotype.Service;
@@ -11,46 +12,54 @@ import java.util.Optional;
 public class JusticeLeagueService {
 
     private JusticeLeagueRepository justiceLeagueRepository;
+    private ComicDetailsService comicDetailsService;
 
-    public JusticeLeagueService(JusticeLeagueRepository justiceLeagueRepository){
+    public JusticeLeagueService(JusticeLeagueRepository justiceLeagueRepository, ComicDetailsService comicDetailsService){
         this.justiceLeagueRepository = justiceLeagueRepository;
+        this.comicDetailsService = comicDetailsService;
     }
 
     public List<SuperHeroes> findAllSuperHero() throws Exception {
         return this.justiceLeagueRepository.findAll();
     }
 
-    public SuperHeroes findSuperHero(String name) throws Exception {
-        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findBySuperHeroName(name);
+    public SuperHeroes findSuperHero(String superHeroId) throws Exception {
+        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findById(superHeroId);
         if(!existing.isPresent()){
-            throw new Exception("Bad Request");
+            throw new Exception("SuperHero Not Exists");
         }
         return existing.get();
     }
 
     public SuperHeroes createSuperHero(SuperHeroes superHeroes) throws Exception {
-        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findBySuperHeroName(superHeroes.getSuperHeroName());
+        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findById(superHeroes.getId());
         if(existing.isPresent()){
-            throw new Exception("Bad Request");
+            throw new Exception("SuperHero Exists");
         }
+
+        ComicDetails existingComic = this.comicDetailsService.findByComicId(superHeroes.getComicDetailsId());
+        if(existingComic.getStatus().equals(Boolean.FALSE)) {
+            throw new Exception("Comic is Disabled, SuperHero creation failed");
+        }
+
         return this.justiceLeagueRepository.save(superHeroes);
     }
 
-    public SuperHeroes updateSuperHero(String superHeroNme, SuperHeroes superHeroes) throws Exception {
-        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findBySuperHeroName(superHeroNme);
+    public SuperHeroes updateSuperHero(String superHeroId, SuperHeroes superHeroes) throws Exception {
+        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findById(superHeroId);
         if(!existing.isPresent()){
             throw new Exception("SuperHero Not Found");
         }
-        superHeroes.setId(superHeroNme);
+        superHeroes.setId(superHeroId);
         return this.justiceLeagueRepository.save(superHeroes);
     }
 
-    public void deleteSuperHero(String superHeroNme) throws Exception {
-        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findBySuperHeroName(superHeroNme);
+    public void deleteSuperHero(String superHeroId) throws Exception {
+        Optional<SuperHeroes> existing = this.justiceLeagueRepository.findById(superHeroId);
         if(!existing.isPresent()){
             throw new Exception("SuperHero Not Found");
         }
-        this.justiceLeagueRepository.deleteById(superHeroNme);
+        this.justiceLeagueRepository.deleteById(superHeroId);
     }
 
 
--- src/test/java/com/svgagan/justiceleague/JusticeLeagueApplicationTests.java
+++ src/test/java/com/svgagan/justiceleague/JusticeLeagueApplicationTests.java
@@ -3,6 +3,12 @@ package com.svgagan.justiceleague;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+	/**
+	*
+	* To exclude jdbc auto-configurations.
+	* https://www.baeldung.com/spring-boot-exclude-auto-configuration-test
+	* */
+
 @SpringBootTest
 class JusticeLeagueApplicationTests {
 
